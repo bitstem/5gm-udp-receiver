c50f628c9ece92f45e2fbd2744596f8c40158e9e
Klaus Kuehnhammer <klaus@bitstem.com>
2024-06-05 18:50:34 +0200

    show delta, don't expect sample offset scaling

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <chrono>
 
 #include <boost/asio.hpp>
 #include <boost/exception/diagnostic_information.hpp> 
 
-#undef  VERBOSE
+#define  VERBOSE
 #define BUFFERSIZE 2048
 
 // undef the following for unicast reception
@@ -33,7 +34,8 @@ int main(){
   unsigned last_sample_offset = 0;
   unsigned last_ms = 0;
   unsigned last_s = 0;
-  unsigned expected_increase = 640;
+  unsigned expected_increase = 320;
+  unsigned nr_packets = 0;
   while (true){
     try{
       auto bytesRead = socket.receive_from(boost::asio::buffer(buffer, BUFFERSIZE), senderEndpoint);
@@ -55,15 +57,26 @@ int main(){
         s |= *buf++;
 
 #ifdef VERBOSE
-        std::cout << "Received " << bytesRead << " bytes for s " << s << ", ms " << ms << 
-          ", sample offset " << samples << " from sender " << senderEndpoint.address().to_string() << "\n";
+        if (ms%1000 == 0 && samples == 0) {
+         uint64_t now = (duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())).count();
+         uint64_t const DIFF_UTC_1970_1980 = 315'964'800U;
+         now = (now - (DIFF_UTC_1970_1980*1000)) + 18000;
+
+         int64_t delta = (int64_t)(((int64_t)s*1000L) + ms) - (int64_t)now;
+
+        std::cout << now << ": received " << bytesRead << " bytes for s " << s << ", ms " << ms << 
+          ", sample offset " << samples << " from sender " << senderEndpoint.address().to_string() <<
+          ", " << delta << " ms delta, packet rate: " << nr_packets << "/ms\n";
+
+        nr_packets = 0;
+        }
 #endif
 
         unsigned expected_s = last_s;
         unsigned expected_ms = last_ms;
         unsigned expected_samples = last_sample_offset + expected_increase;
 
-        if (last_sample_offset == 14720) {
+        if (last_sample_offset == 7360) { //15040) {
           expected_samples = 0;
           expected_ms++;
           if (last_ms == 999) {
@@ -83,6 +96,7 @@ int main(){
         last_sample_offset = samples;
         last_ms = ms;
         last_s = s;
+        nr_packets++;
 
       }
     }
